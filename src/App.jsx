574a91ad6cbeb492e5fd83b62c01a68af129439f
alejandra rueda velez <lalita7959@gmail.com>
2024-04-03 19:20:26 -0500

    front end

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+import { Outlet } from 'react-router-dom'
+import NavBar from './components/NavBar'
+import { Container, Modal } from 'react-bootstrap'
+import LoadingScreen from './components/LoadingScreen'
+import { useSelector } from 'react-redux'
+import { useState } from 'react'
+
+function App() {
+
+  const isLoading = useSelector(state => state.app.isLoading);
+
+  return (
+    <>
+      
+      <NavBar />
+      <Container className='py-5'>
+        <Outlet />
+      </Container>
+      { isLoading && <LoadingScreen /> }
+    </>
+  )
+}
+
+export default App
